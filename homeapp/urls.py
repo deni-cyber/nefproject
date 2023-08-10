f5f56f4eb6b795b25459f65c3040422bd0138cc9
deni-cyber <deniskamau928@gmail.com>
2023-08-10 20:35:39 +0300

    updated urls for service and product html

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("about/", views.about_view, name='about'),
     path("projects/", views.projects_view, name='projects'),
     path("contact/", views.contact_view, name='contact'),
+    path("products/", views.contact_view, name='products'),
+    path("services/", views.contact_view, name='services'),
+
 ]
 
